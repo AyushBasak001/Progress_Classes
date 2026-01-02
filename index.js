89eb427a0987505c3329525009408b65b5816faf
AyushBasak001 <ayushbasak31@gmail.com>
2026-01-02 20:09:22 +0530

    Admin Enquiry backend route setup

--- index.js
+++ index.js
@@ -19,13 +19,15 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Public Section
+
 app.get('/', (req, res) => {
   res.send("Progress Classes Backend Running");
 });
 
 app.get("/course", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM course ORDER BY name ASC");
+    const result = await db.query("SELECT fc.course_id, c.name AS course_name, c.level, fc.faculty_id, f.first_name, f.last_name FROM faculty_course fc JOIN faculty f ON fc.faculty_id = f.id JOIN course c ON fc.course_id = c.id ORDER BY c.name ASC, CASE c.level WHEN 'beginner' THEN 1 WHEN 'intermediate' THEN 2 WHEN 'advanced' THEN 3 END ASC");
     const courses = result.rows;
     res.json(courses);
   } catch (err) {
@@ -36,7 +38,7 @@ app.get("/course", async (req, res) => {
 
 app.get("/faculty", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM faculty ORDER BY first_name ASC, last_name ASC");
+    const result = await db.query("SELECT fc.faculty_id, f.first_name, f.last_name, f.qualification, f.date_joined, fc.course_id, c.name AS course_name, c.level FROM faculty_course fc JOIN faculty f ON fc.faculty_id = f.id JOIN course c ON fc.course_id = c.id ORDER BY f.first_name ASC, f.last_name ASC");
     const faculties = result.rows;
     res.json(faculties);
   } catch (err) {
@@ -47,9 +49,103 @@ app.get("/faculty", async (req, res) => {
 
 app.get("/enquiry", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM enquiry");
+    const result = await db.query("SELECT * FROM enquiry WHERE is_visible AND is_answered ORDER BY created_at ASC");
     const enquiries = result.rows;
     res.json(enquiries);
+  } catch (err) {
+    console.error("Error executing Query : ", err);
+    res.json({'error': err});
+  }
+});
+
+app.post("/enquiry", async (req, res) => {
+  const name = req.body.name;
+  const question = req.body.question;
+  try {
+    const result = await db.query("INSERT INTO enquiry (name, question) VALUES($1,$2) RETURNING *",[name,question]);
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error("Error executing Query : ", err);
+    res.json({'error': err});
+  }
+});
+
+// Admin Section
+
+app.get('/admin-login', (req, res) => {
+  res.send("Admin Login page");
+});
+
+app.get('/admin', (req, res) => {
+  res.send("Welcome admin");
+});
+
+// Admin -> course Section
+
+app.get("/admin/course", async (req, res) => {
+  try {
+    const result = await db.query("SELECT fc.course_id, c.name AS course_name, c.level, fc.faculty_id, f.first_name, f.last_name FROM faculty_course fc JOIN faculty f ON fc.faculty_id = f.id JOIN course c ON fc.course_id = c.id ORDER BY c.name ASC, CASE c.level WHEN 'beginner' THEN 1 WHEN 'intermediate' THEN 2 WHEN 'advanced' THEN 3 END ASC");
+    const courses = result.rows;
+    res.json(courses);
+  } catch (err) {
+    console.error("Error executing Query : ", err);
+    res.json({'error': err});
+  }
+});
+
+// Admin -> faculty Section
+
+app.get("/admin/faculty", async (req, res) => {
+  try {
+    const result = await db.query("SELECT fc.faculty_id, f.first_name, f.last_name, f.qualification, f.date_joined, fc.course_id, c.name AS course_name, c.level FROM faculty_course fc JOIN faculty f ON fc.faculty_id = f.id JOIN course c ON fc.course_id = c.id ORDER BY f.first_name ASC, f.last_name ASC");
+    const faculties = result.rows;
+    res.json(faculties);
+  } catch (err) {
+    console.error("Error executing Query : ", err);
+    res.json({'error': err});
+  }
+});
+
+// Admin -> enquiry Section
+
+app.get("/admin/enquiry", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM enquiry ORDER BY created_at ASC");
+    const enquiries = result.rows;
+    res.json(enquiries);
+  } catch (err) {
+    console.error("Error executing Query : ", err);
+    res.json({'error': err});
+  }
+});
+
+app.patch("/admin/enquiry/:id", async (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  if('answer' in req.body){
+    try {
+      const result = await db.query("UPDATE enquiry SET answer = $1, is_answered = true, answered_at = CURRENT_TIMESTAMP, is_visible = $2 WHERE id = $3",[req.body.answer,req.body.is_visible,req.params.id]);
+      res.json(result.rows[0]);
+    } catch (err) {
+      console.error("Error executing Query : ", err);
+      res.json({'error': err});
+    }
+  } else{
+    try {
+      const result = await db.query("UPDATE enquiry SET answer = null, is_answered = false, answered_at = null, is_visible = $1 WHERE id = $2",[req.body.is_visible,req.params.id]);
+      res.json(result.rows[0]);
+    } catch (err) {
+      console.error("Error executing Query : ", err);
+      res.json({'error': err});
+    }
+  }
+});
+
+app.delete("/admin/enquiry/:id", async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const result = await db.query("DELETE FROM enquiry WHERE id = $1",[req.params.id]);
+    res.json(result.rowCount);
   } catch (err) {
     console.error("Error executing Query : ", err);
     res.json({'error': err});
